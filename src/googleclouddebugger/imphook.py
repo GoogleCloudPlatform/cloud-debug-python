c1d10c5fa9e438efebe85a4163846ab82cb5df78
erezh <erezh@google.com>
2017-10-16 13:36:19 -0700

    Address import callback invoke for: 1. Premature callback invoke, before module is fully loaded. This happens in nested imports where the package callback is invoked prematurely. 2. When a module/package includes the __all__ attribute indicating modules to load.
    This CL invokes the import callbacks only on the outer import call per thread.
    This CL looks up if a module matching *any* of the pending callbacks is loaded, regardless of the actually imported name.
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=172370587

--- src/googleclouddebugger/imphook.py
+++ src/googleclouddebugger/imphook.py
@@ -24,6 +24,9 @@ import module_utils
 _import_callbacks = {}
 _import_callbacks_lock = threading.Lock()
 
+# Per thread data holding information about the import call nest level.
+_import_local = threading.local()
+
 # Original __import__ function if import hook is installed or None otherwise.
 _real_import = None
 
@@ -94,113 +97,34 @@ def _ImportHook(name, globals=None, locals=None, fromlist=None, level=-1):
   to __import__ that use keyword syntax will fail: __import('a', fromlist=[]).
   """
 
-  # Really import modules.
-  module = _real_import(name, globals, locals, fromlist, level)
+  # This is the top call to import (no nesting), init the per-thread nest level.
+  if getattr(_import_local, 'nest_level', None) is None:
+    _import_local.nest_level = 0
+
+  _import_local.nest_level += 1
+
+  try:
+    # Really import modules.
+    module = _real_import(name, globals, locals, fromlist, level)
+  finally:
+    _import_local.nest_level -= 1
+
+  # No need to invoke the callbacks on nested import calls.
+  if _import_local.nest_level:
+    return module
 
   # Optimize common code path when no breakponts are set.
   if not _import_callbacks:
     return module
 
-  # When the _real_import statement above is executed, it can also trigger the
-  # loading of outer packages, if they are not loaded yet. Unfortunately,
-  # _real_import does not give us a list of packages/modules were loaded as
-  # a result of executing it. Therefore, we conservatively assume that they
-  # were all just loaded.
-  #
-  # To manually identify all modules that _real_import touches, we apply a
-  # method that combines 'module', 'name', and 'fromlist'. This method is a
-  # heuristic that is based on observation.
-  #
-  # Note that the list we obtain will contain false positives, i.e., modules
-  # that were already loaded. However, since these modules were already loaded,
-  # there can be no pending breakpoint callbacks on them, and therefore, the
-  # wasted computation will be limited to one dictionary lookup per module.
-  #
-  # Example: When module 'a.b.c' is imported, we need to activate deferred
-  # breakpoints in all of ['a', 'a.b', 'a.b.c']. If 'a' was already loaded, then
-  # _import_callbacks.get('a') will return nothing, and we will move on to
-  # 'a.b'.
-  #
-  # To make the code simpler, we keep track of parts of the innermost module
-  # (i.e., 'a', 'b', 'c') and then combine them later.
-
-  parts = module.__name__.split('.')
-  if fromlist:
-    # In case of 'from x import y', all modules in 'fromlist' can be directly
-    # found in the package identified by the returned 'module'.
-    # Note that we discard the 'name' field, because it is a substring of the
-    # name of the returned module.
-
-    # Example 1: Using absolute path.
-    #     from a.b import c
-    #     name = 'a.b', fromlist=['c'], module=<module 'a.b'>
-    #
-    # Example 2: Using relative path from inside package 'a'.
-    #     from b import c
-    #     name = 'b', fromlist=['c'], module=<module 'a.b'>
-    #
-    # Example 3: Using relative path from inside package 'a'.
-    #    from b.c import d
-    #    name = 'b.c', fromlist=['d'], module=<module 'a.b.c'>
-    pass
-  else:
-    # In case of 'import a.b', we append the 'name' field to the name of the
-    # returned module. Note that these two have one component in common, so
-    # we remove that one component from the start of 'name' before appending it.
-
-    # Example 1: Use absolute path.
-    #    import a
-    #    name = 'a', fromlist=None, module=<module 'a'>
-    #
-    # Example 2: Use absolute path.
-    #    import a.b
-    #    name = 'a.b', fromlist=None, module=<module 'a'>
-    #
-    # Example 3: Use absolute path.
-    #    import a.b.c.d
-    #    name = 'a.b.c.d', fromlist=None, module=<module 'a'>
-    #
-    # Example 4: Use relative path from inside package 'a'.
-    #    import b.c
-    #    name = 'b.c', fromlist=None, module='a.b'
-    parts += name.split('.')[1:]
-
-  def GenerateModules():
-    """Generates module names using parts and fromlist."""
-    # If parts contains ['a', 'b', 'c'], then we generate ['a', 'a.b','a.b.c'].
-    current = None
-    for part in parts:
-      current = (current + '.' + part) if current else part
-      yield current
-
-    # We then add entries in fromlist to the final package path (i.e., 'a.b.c')
-    # to obtain the innermost packages (i.e., 'a.b.c.d, a.b.c.e').
-    if fromlist:
-      for f in fromlist:
-        yield current + '.' + f
-
-  for m in GenerateModules():
-    _InvokeImportCallback(sys.modules.get(m))
-
+  _InvokeImportCallback()
   return module
 
 
-def _InvokeImportCallback(module):
-  """Invokes import callbacks for the specified module."""
-
-  if not module:
-    return
-
-  mod_path = getattr(module, '__file__', None)
-  if not mod_path:
-    return
-
-  mod_abspath = module_utils.GetAbsolutePath(mod_path)
-  mod_abspath, unused_ext = os.path.splitext(mod_abspath)
-  callbacks = _import_callbacks.get(mod_abspath)
-  if not callbacks:
-    return  # Common code path.
-
-  # Clone the callbacks set, since it can change during enumeration.
-  for callback in callbacks.copy():
-    callback(module)
+def _InvokeImportCallback():
+  """Invokes import callbacks for loaded modules."""
+  for path, callbacks in _import_callbacks.items():
+    module = module_utils.GetLoadedModuleByPath(path)
+    if module:
+      for callback in callbacks.copy():
+        callback(module)
