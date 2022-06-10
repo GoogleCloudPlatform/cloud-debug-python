7b144dd81ed34cc8aa1fbfd733290d4f314e41bf
James McTavish <mctavish@google.com>
2022-06-10 15:58:28 +0000

    Add final python unit tests.

---
+++ tests/uniquifier_computer_test.py
@@ -0,0 +1,125 @@
+"""Unit test for uniquifier_computer module."""
+
+import os
+import sys
+import tempfile
+
+from absl.testing import absltest
+
+from googleclouddebugger import uniquifier_computer
+
+
+class UniquifierComputerTest(absltest.TestCase):
+
+  def _Compute(self, files):
+    """Creates a directory structure and computes uniquifier on it.
+
+    Args:
+      files: dictionary of relative path to file content.
+
+    Returns:
+      Uniquifier data lines.
+    """
+
+    class Hash(object):
+      """Fake implementation of hash to collect raw data."""
+
+      def __init__(self):
+        self.data = b''
+
+      def update(self, s):
+        self.data += s
+
+    root = tempfile.mkdtemp('', 'fake_app_', absltest.get_default_test_tmpdir())
+    for relative_path, content in files.items():
+      path = os.path.join(root, relative_path)
+      directory = os.path.split(path)[0]
+      if not os.path.exists(directory):
+        os.makedirs(directory)
+      with open(path, 'w') as f:
+        f.write(content)
+
+    sys.path.insert(0, root)
+    try:
+      hash_obj = Hash()
+      uniquifier_computer.ComputeApplicationUniquifier(hash_obj)
+      return [
+          u.decode() for u in (
+              hash_obj.data.rstrip(b'\n').split(b'\n') if hash_obj.data else [])
+      ]
+    finally:
+      del sys.path[0]
+
+  def testEmpty(self):
+    self.assertListEqual(
+        [],
+        self._Compute({}))
+
+  def testBundle(self):
+    self.assertListEqual(
+        ['first.py:1',
+         'in1/__init__.py:6',
+         'in1/a.py:3',
+         'in1/b.py:4',
+         'in1/in2/__init__.py:7',
+         'in1/in2/c.py:5',
+         'second.py:2'],
+        self._Compute({
+            'db.app': 'abc',
+            'first.py': 'a',
+            'second.py': 'bb',
+            'in1/a.py': 'ccc',
+            'in1/b.py': 'dddd',
+            'in1/in2/c.py': 'eeeee',
+            'in1/__init__.py': 'ffffff',
+            'in1/in2/__init__.py': 'ggggggg'}))
+
+  def testEmptyFile(self):
+    self.assertListEqual(
+        ['empty.py:0'],
+        self._Compute({
+            'empty.py': ''}))
+
+  def testNonPythonFilesIgnored(self):
+    self.assertListEqual(
+        ['real.py:1'],
+        self._Compute({
+            'file.p': '',
+            'file.pya': '',
+            'real.py': '1'}))
+
+  def testNonPackageDirectoriesIgnored(self):
+    self.assertListEqual(
+        ['dir2/__init__.py:1'],
+        self._Compute({
+            'dir1/file.py': '',
+            'dir2/__init__.py': 'a',
+            'dir2/image.gif': ''}))
+
+  def testDepthLimit(self):
+    self.assertListEqual(
+        [''.join(str(n) + '/' for n in range(1, m + 1)) + '__init__.py:%d' % m
+         for m in range(9, 0, -1)],
+        self._Compute({
+            '1/__init__.py': '1',
+            '1/2/__init__.py': '2' * 2,
+            '1/2/3/__init__.py': '3' * 3,
+            '1/2/3/4/__init__.py': '4' * 4,
+            '1/2/3/4/5/__init__.py': '5' * 5,
+            '1/2/3/4/5/6/__init__.py': '6' * 6,
+            '1/2/3/4/5/6/7/__init__.py': '7' * 7,
+            '1/2/3/4/5/6/7/8/__init__.py': '8' * 8,
+            '1/2/3/4/5/6/7/8/9/__init__.py': '9' * 9,
+            '1/2/3/4/5/6/7/8/9/10/__init__.py': 'a' * 10,
+            '1/2/3/4/5/6/7/8/9/10/11/__init__.py': 'b' * 11}))
+
+  def testPrecedence(self):
+    self.assertListEqual(
+        ['my.py:3'],
+        self._Compute({
+            'my.pyo': 'a',
+            'my.pyc': 'aa',
+            'my.py': 'aaa'}))
+
+if __name__ == '__main__':
+  absltest.main()
---
+++ tests/yaml_data_visibility_config_reader_test.py
@@ -0,0 +1,117 @@
+"""Tests for yaml_data_visibility_config_reader."""
+
+import os
+import sys
+from unittest import mock
+
+from six import StringIO
+
+from absl.testing import absltest
+from googleclouddebugger import yaml_data_visibility_config_reader
+
+
+class StringIOOpen(object):
+  """An open for StringIO that supports "with" semantics.
+
+  I tried using mock.mock_open, but the read logic in the yaml.load code is
+  incompatible with the returned mock object, leading to a test hang/timeout.
+  """
+
+  def __init__(self, data):
+    self.file_obj = StringIO(data)
+
+  def __enter__(self):
+    return self.file_obj
+
+  def __exit__(self, type, value, traceback):  # pylint: disable=redefined-builtin
+    pass
+
+
+class YamlDataVisibilityConfigReaderTest(absltest.TestCase):
+
+  def testOpenAndReadSuccess(self):
+    data = """
+      blacklist:
+        - bl1
+    """
+    path_prefix = 'googleclouddebugger.'
+    with mock.patch(path_prefix + 'yaml_data_visibility_config_reader.open',
+                    create=True) as m:
+      m.return_value = StringIOOpen(data)
+      config = yaml_data_visibility_config_reader.OpenAndRead()
+      m.assert_called_with(os.path.join(sys.path[0], 'debugger-blacklist.yaml'),
+                           'r')
+      self.assertEqual(config.blacklist_patterns, ['bl1'])
+
+  def testOpenAndReadFileNotFound(self):
+    path_prefix = 'googleclouddebugger.'
+    with mock.patch(path_prefix + 'yaml_data_visibility_config_reader.open',
+                    create=True, side_effect=IOError('IO Error')):
+      f = yaml_data_visibility_config_reader.OpenAndRead()
+      self.assertIsNone(f)
+
+  def testReadDataSuccess(self):
+    data = """
+      blacklist:
+        - bl1
+        - bl2
+      whitelist:
+        - wl1
+        - wl2.*
+    """
+
+    config = yaml_data_visibility_config_reader.Read(StringIO(data))
+    self.assertItemsEqual(config.blacklist_patterns, ('bl1', 'bl2'))
+    self.assertItemsEqual(config.whitelist_patterns, ('wl1', 'wl2.*'))
+
+  def testYAMLLoadError(self):
+    class ErrorIO(object):
+
+      def read(self, size):
+        del size  # Unused
+        raise IOError('IO Error')
+
+    with self.assertRaises(yaml_data_visibility_config_reader.YAMLLoadError):
+      yaml_data_visibility_config_reader.Read(ErrorIO())
+
+  def testBadYamlSyntax(self):
+    data = """
+      blacklist: whitelist:
+    """
+
+    with self.assertRaises(yaml_data_visibility_config_reader.ParseError):
+      yaml_data_visibility_config_reader.Read(StringIO(data))
+
+  def testUnknownConfigKeyError(self):
+    data = """
+      foo:
+        - bar
+    """
+
+    with self.assertRaises(
+        yaml_data_visibility_config_reader.UnknownConfigKeyError):
+      yaml_data_visibility_config_reader.Read(StringIO(data))
+
+  def testNotAListError(self):
+    data = """
+      blacklist:
+        foo:
+          - bar
+    """
+
+    with self.assertRaises(yaml_data_visibility_config_reader.NotAListError):
+      yaml_data_visibility_config_reader.Read(StringIO(data))
+
+  def testElementNotAStringError(self):
+    data = """
+      blacklist:
+        - 5
+    """
+
+    with self.assertRaises(
+        yaml_data_visibility_config_reader.ElementNotAStringError):
+      yaml_data_visibility_config_reader.Read(StringIO(data))
+
+
+if __name__ == '__main__':
+  absltest.main()
