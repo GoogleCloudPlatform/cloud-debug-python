8bff95ada04dc9e42d68d316291d58a92b9cfa4d
James McTavish <mctavish@google.com>
2022-06-14 15:04:19 -0400

    Update build.sh to use pip to build.

--- src/build.sh
+++ src/build.sh
@@ -85,6 +85,6 @@ library_dirs=${ROOT}/build/third_party/lib" > ${ROOT}/setup.cfg
 # Build the Python Cloud Debugger agent.
 pushd ${ROOT}
 # Use custom python command if variable is set
-"${PYTHON:-python3}" setup.py bdist_wheel
+"${PYTHON:-python3}" -m pip wheel . --no-deps -w dist
 popd
 
