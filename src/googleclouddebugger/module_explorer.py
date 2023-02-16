fcf305825c3bfa4178725f41920d44fdb8e0ca3a
James McTavish <mctavish@google.com>
2023-02-16 14:09:49 -0500

    a slightly more complete fix

--- src/googleclouddebugger/module_explorer.py
+++ src/googleclouddebugger/module_explorer.py
@@ -62,7 +62,6 @@ def GetCodeObjectAtLine(module, line):
         prev_line = max(prev_line, co_line_number)
       elif co_line_number > line:
         next_line = min(next_line, co_line_number)
-        break
 
   prev_line = None if prev_line == 0 else prev_line
   next_line = None if next_line == sys.maxsize else next_line
