c2cf833633bc7a59b90c8d1581776924ab6f6156
Jason Borg <jcborg@google.com>
2023-03-02 16:34:01 +0000

    chore: Add extra tess cleanup to build scripts
    This addresses an issue where first running `./build_and_test.sh`
    followed by `build_dist.sh` could see some test failures while running
    the seconde script. One concrete scenario was when the local python
    version was 3.10.9 while version 3.10.10 was used during
    `build_dist.sh`.

--- build_and_test.sh
+++ build_and_test.sh
@@ -1,5 +1,8 @@
 #!/bin/bash -e
 
+# Clean up any previous generated test files.
+rm -rf tests/py/__pycache__
+
 cd src
 ./build.sh
 cd ..
@@ -10,3 +13,6 @@ pip3 install -r requirements_dev.txt
 pip3 install src/dist/* --force-reinstall
 python3 -m pytest tests/py
 deactivate
+
+# Clean up any generated test files.
+rm -rf tests/py/__pycache__
